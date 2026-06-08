1544a882a11db7775f7138d169952cd3f6ea640a
Andhika Kurniawan <andhika.kurniawan@adira.co.id>
2026-06-08 11:45:06 +0700

    build: setup eslint and prettier

--- src/app/RootNavigator.tsx
+++ src/app/RootNavigator.tsx
@@ -1,14 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import AccountManagerScreen from "../features/account/screens/AccountManagerScreen";
+import AccountManagerScreen from "@/features/account/screens/AccountManagerScreen";
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
         <Stack.Screen name="AccountManager" component={AccountManagerScreen} />
       </Stack.Navigator>
     </NavigationContainer>
