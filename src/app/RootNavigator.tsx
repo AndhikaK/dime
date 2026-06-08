8c28aad97417efcc1880c397959373507ccf6bf0
Andhika Kurniawan <andhika.kurniawan@adira.co.id>
2026-06-08 11:31:15 +0700

    build: setup react-navigation

---
+++ src/app/RootNavigator.tsx
@@ -0,0 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import AccountManagerScreen from "../features/account/screens/AccountManagerScreen";
+
+const Stack = createNativeStackNavigator();
+
+const RootNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="AccountManager" component={AccountManagerScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigator;
---
+++ src/features/account/screens/AccountManagerScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const AccountManagerScreen = () => {
+  return (
+    <View>
+      <Text>Account Manager Screen</Text>
+    </View>
+  );
+};
+
+export default AccountManagerScreen;
