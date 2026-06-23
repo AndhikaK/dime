6263aa6fb7c937eee111d6a2677f394756ae81f4
Andhika Kurniawan <andhika.kurniawan@adira.co.id>
2026-06-23 13:45:31 +0700

    style: setup eslint-plugin-simple-import-sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,42 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
+// eslint.config.js
+const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
+
   {
     ignores: ["dist/*"],
-  }
+
+    plugins: {
+      "simple-import-sort": require("eslint-plugin-simple-import-sort"),
+    },
+
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // 1. Side effect imports
+            ["^\\u0000"],
+
+            // 2. react + packages
+            ["^react$", "^@?\\w"],
+
+            // 3. absolute + alias + others
+            ["^@", "^"],
+
+            // 4. relative imports
+            ["^\\./"],
+
+            // 5. style modules
+            ["^.+\\.(module\\.css|module\\.scss)$"],
+
+            // 6. media assets
+            ["^.+\\.(gif|png|svg|jpg)$"],
+          ],
+        },
+      ],
+    },
+  },
 ]);
